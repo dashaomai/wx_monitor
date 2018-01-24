6acb30a8da26ce072045a207194cd994615fb083
Bob Jiang <dashaomai@qq.com>
2018-01-24 16:26:53 +0800

    修正了代码格式，添加了新的数据模型设计

--- src/db.py
+++ src/db.py
@@ -1,51 +1,63 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import mysql.connector
 
-dbConfig = {'user': 'root', 'password': 'docker', 'database': 'wx_monitor'}
+db_config = {'user': 'root', 'password': 'docker', 'database': 'wx_monitor'}
 
-def _selectChatrooms(_, cursor):
+
+def _select_chatrooms(_, cursor):
     cursor.execute('SELECT * FROM chatrooms;')
     values = cursor.fetchall()
 
-    #return [x[0] for x in values]
+    # return [x[0] for x in values]
     return values
 
-# 选出要监控的聊天群名称
-def selectChatrooms():
-    return _dbTemplate(_selectChatrooms)
 
-def _insertTextMessage(conn, cursor, nickname, content, createTime):
-    cursor.execute('INSERT INTO text_messages (create_time, nickname, content) VALUES (%s, %s, %s);', [createTime, nickname, content])
+# 选出要监控的聊天群名称
+def select_chatrooms():
+    return _db_template(_select_chatrooms)
+
+
+def _insert_text_message(conn, cursor, nickname, content, create_time):
+    cursor.execute(
+	    'INSERT INTO text_messages (create_time, nickname, content) VALUES (%s, %s, %s);',
+      [create_time, nickname, content]
+    )
 
     conn.commit()
 
     return 1 == cursor.rowcount
+
 
 # 插入一条聊天内容
-def insertTextMessage(nickname, content, createTime):
-    return _dbTemplate(_insertTextMessage, nickname=nickname, content=content, createTime=createTime)
+def insert_text_message(nickname, content, create_time):
+    return _db_template(_insert_text_message, nickname=nickname, content=content, createTime=create_time)
 
-def _insertRecordingMessage(conn, cursor, nickname, filename, createTime):
-    cursor.execute('INSERT INTO recording_messages (create_time, nickname, filename) VALUES(%s, %s, %s);', [createTime, nickname, filename])
+
+def _insert_recording_message(conn, cursor, nickname, filename, createTime):
+    cursor.execute(
+	    'INSERT INTO recording_messages (create_time, nickname, filename) VALUES(%s, %s, %s);',
+	    [createTime, nickname, filename]
+    )
 
     conn.commit()
 
     return 1 == cursor.rowcount
 
-# 插入一条语音记录
-def insertRecordingMessage(nickname, filename, createTime):
-    return _dbTemplate(_insertRecordingMessage, nickname=nickname, filename=filename, createTime=createTime)
 
-def _dbTemplate(callback, **other):
+# 插入一条语音记录
+def insert_recording_message(nickname, filename, create_time):
+    return _db_template(_insert_recording_message, nickname=nickname, filename=filename, createTime=create_time)
+
+
+def _db_template(callback, **other):
     try:
-        conn = mysql.connector.connect(**dbConfig)
+        conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
 
-        cursor.execute('SET NAMES utf8mb4');
-        cursor.execute('SET CHARACTER SET utf8mb4');
-        cursor.execute('SET character_set_connection=utf8mb4');
+        cursor.execute('SET NAMES utf8mb4')
+        cursor.execute('SET CHARACTER SET utf8mb4')
+        cursor.execute('SET character_set_connection=utf8mb4')
 
         return callback(conn, cursor, **other)
 
--- src/wx_monitor.py
+++ src/wx_monitor.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import itchat
@@ -6,35 +6,35 @@ from itchat.content import *
 from db import *
 
 # 从 MySQL 中读取要监控的聊天室的名字
-groups = selectChatrooms()
+groups = select_chatrooms()
 if len(groups) > 0:
     group = groups[0]
-    groupName = group[1]
+    group_name = group[1]
 
     # 以字符界面显示登录用二维码
-    itchat.auto_login(enableCmdQR=2)
+    itchat.auto_login(enableCmdQR=True)
 
-    groups = itchat.search_chatrooms(name=groupName)
+    groups = itchat.search_chatrooms(name=group_name)
     if len(groups) > 0:
         group = groups[0]
-        #print(group)
-        groupUserName = group['UserName']
+        # print(group)
+        group_user_name = group['UserName']
 
         # 保存文字消息
         @itchat.msg_register([TEXT], isGroupChat=True)
         def print_messages(msg):
-            groupName2 = msg['User']['NickName']
-            if groupName2 == groupName:
-                #print('符合')
+            group_name2 = msg['User']['NickName']
+            if group_name2 == group_name:
+                # print('符合')
                 nickname = msg['ActualNickName']
                 content = msg['Content']
-                createTime = msg['CreateTime']
-                insertTextMessage(nickname, content, createTime)
+                create_time = msg['CreateTime']
+                insert_text_message(nickname, content, create_time)
 
                 print('消息 ' + content + '已保存')
 
-            #else:
-                #print('不符')
+            # else:
+                # print('不符')
 
         # 保存语音消息
         @itchat.msg_register([RECORDING], isGroupChat=True)
@@ -43,12 +43,12 @@ if len(groups) > 0:
 
             nickname = msg['ActualNickName']
             filename = msg.fileName
-            createTime = msg['CreateTime']
-            insertRecordingMessage(nickname, filename, createTime)
+            create_time = msg['CreateTime']
+            insert_recording_message(nickname, filename, create_time)
 
             print('文件 ' + msg.fileName + ' 已下载')
     else:
-        print('未能找到名为：' + groupName + '的聊天群')
+        print('未能找到名为：' + group_name + '的聊天群')
 
 else:
     print('无法从数据库内加载要监视的微信群')
